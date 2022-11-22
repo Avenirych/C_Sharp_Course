c560ea7e9492ad52299008accd708f762a1c2a55
Sergey <sergeileonov@inbox.ru>
2022-11-22 22:47:53 +0000

    Days of week

---
+++ 1_Lesson 1.2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Write a number 2");
+int y = int.Parse(Console.ReadLine());
+
+if(y == 1)
+{
+    Console.WriteLine("Monday");
+}
+
+else if (y == 2)
+{
+    Console.WriteLine("Tusday");
+}
