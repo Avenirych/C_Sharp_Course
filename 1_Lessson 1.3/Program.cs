3f4ff7a3b0d60474029607af9095897dba5dd450
Sergey <sergeileonov@inbox.ru>
2022-11-23 10:57:38 +0000

    6->6  -82->82

---
+++ 1_Lessson 1.3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Show N as interval -N To N
+
+Console.WriteLine("Write a number; ");
+string a = Console.ReadLine();
+int N = int.Parse(a);
+int NegN = - N;
+
+while(NegN <= N)
+{
+    Console.Write($"{NegN} ");
+    NegN +=1;
+}
