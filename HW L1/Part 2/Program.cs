38404873f9c2ffb783588ccd6a07b9dc3daee002
Sergey <sergeileonov@inbox.ru>
2022-11-24 17:38:03 +0000

    Part 2

---
+++ HW L1/Part 2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Write the first number: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Write the second number: ");
+int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Write the third number: ");
+int num3 = int.Parse(Console.ReadLine());
+int max = num1;
+
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+Console.WriteLine();
+Console.WriteLine($"max = {max}");
