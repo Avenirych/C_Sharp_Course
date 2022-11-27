e1ecddd5e0fa8a9cba768e70511ba11360a81bbc
Sergey <sergeileonov@inbox.ru>
2022-11-27 22:23:41 +0000

    Write the number and check to it to divisible that to 7 and 23

---
+++ 2_Lesson/2.0/2.3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Write the number and chec to it to divisible that to 7 and 23
+string divisible(int num)
+{
+    if (num % 7 == 0 && num % 23 == 0)
+        return "divisible";
+    else 
+        return "nondivisible";
+}
+Console.WriteLine("Write number");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine(divisible(number));
