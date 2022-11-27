6dd6704ee7d4c551d4b2cf6ff82846f6041a1c2b
Sergey <sergeileonov@inbox.ru>
2022-11-27 21:35:51 +0000

    Add Write two nambers and show that 1 namber multiple for 2

---
+++ 2_Lesson/2.0/2.2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Write two nambers and show that 1 namber multiple for 2
+string crat(int num1, int num2)
+{
+    if (num1 % 2 == 0)
+        return "multiple";
+    else 
+        return $"Nonmultiple, remains = {num1 % num2}";
+}
+Console.WriteLine("1 number");
+int First = int.Parse(Console.ReadLine());
+Console.WriteLine("2 number");
+int Second = int.Parse(Console.ReadLine());
+Console.WriteLine(crat(First,Second));
