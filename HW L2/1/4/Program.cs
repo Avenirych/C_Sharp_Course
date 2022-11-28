cb37dee179ca2c1713b208100ae09910c900f6b1
Sergey <sergeileonov@inbox.ru>
2022-11-28 11:10:08 +0000

    принимает на вход цифру,обозначающую день недели, и проверяет, является ли этот день выходным.

---
+++ HW L2/1/4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход цифру,обозначающую день недели, и проверяет, является ли этот день выходным.
+string Weekdays(int data)
+{
+    if (data == 6 || data == 7)
+        return "wekends";
+    else
+        return "weekday";           
+}
+
+Console.WriteLine("Write number:");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine(Weekdays(num));
