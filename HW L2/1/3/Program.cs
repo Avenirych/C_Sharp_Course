1b929071cfa7e0fe25f64d0e21da2b555367a3a0
Sergey <sergeileonov@inbox.ru>
2022-11-28 11:00:41 +0000

    выводит третью цифру заданного числа или сообщает, что третьей цифры нет

---
+++ HW L2/1/3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+string Number(int n)
+{
+    if (n < 100)
+        return "No third number";
+    else if (n > 99 && n < 1000)
+        return $"Number {n % 10}"; 
+    else
+    {
+        while (n > 1000)
+        {
+            n = n / 10;
+        }   
+    }
+        return $"Number {n % 10}";  
+
+}
+
+Console.WriteLine("Write number:");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine(Number(num));
