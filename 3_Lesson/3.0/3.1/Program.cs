88720ea4836030e154504a301608dece6b22facf
Sergey <sergeileonov@inbox.ru>
2022-11-29 15:14:13 +0000

    Take N quarterand show interval of coordinates

---
+++ 3_Lesson/3.0/3.1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Take point coordinate of x and y that more 0 and show in what quarter is it
+void Quarters(string a)
+{
+    if (a == "00")
+        Console.WriteLine("x = 0 or y = 0");
+    else if (a == "1")
+        Console.WriteLine("x > 0 && y > 0");
+    else if (a == "2")
+        Console.WriteLine("x < 0 && y > 0");
+    else if (a == "3")
+        Console.WriteLine("x < 0 && y < 0");
+    else if (a == "4")
+        Console.WriteLine("x < 0 && y < 0");
+    else
+        System.Console.WriteLine("error");
+}
+
+Quarters(System.Console.ReadLine());
+
