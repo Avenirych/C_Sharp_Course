636672f85411fe8bf91f10f5146df29a4bbb491f
Sergey <sergeileonov@inbox.ru>
2022-11-29 14:12:38 +0000

    Take point coordinate of x and y that more 0 and show in what quarter is it

---
+++ 3_Lesson/3.0/Program.cs
@@ -0,0 +1,16 @@
+﻿// Take point coordinate of x and y that more 0 and show in what quarter is it
+void Quarters(int x, int y)
+{
+    if (x == 0 || y == 0)
+        Console.WriteLine("x = 0 or y = 0");
+    else if (x > 0 && y > 0)
+        Console.WriteLine("I");
+    else if (x < 0 && y > 0)
+        Console.WriteLine("II");
+    else if (x < 0 && y < 0)
+        Console.WriteLine("III");
+    else if (x > 0 && y < 0)
+        Console.WriteLine("IV");
+}
+
+Quarters(-10, -9);
