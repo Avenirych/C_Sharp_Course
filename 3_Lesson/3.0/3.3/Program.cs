590ed8f10d0f235e835695f98435c3440b45d185
Sergey <sergeileonov@inbox.ru>
2022-11-29 18:45:32 +0000

    Calculate swuares of numbers of the N-digit

---
+++ 3_Lesson/3.0/3.3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Calculate swuares of numbers of the N-digit
+void Digit(int N)
+{
+    int i = 0;
+    double result;
+    while (i < N)
+    {
+        i += 1;
+        result = Math.Pow(i,2);
+        Console.Write($"{result},");
+    }
+}
+Console.WriteLine("Write number");
+int number = int.Parse(Console.ReadLine());
+Digit(number);
