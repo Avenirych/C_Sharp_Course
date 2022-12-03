f1aaac8dd003d8a1253da0843d8ca525f2a6a484
Sergey <sergeileonov@inbox.ru>
2022-12-03 15:18:28 +0000

    принимает на вход пятизначное число и проверяет, является ли оно палиндромом

--- HW L3/Task 19/Program.cs
+++ HW L3/Task 19/Program.cs
@@ -1,24 +1,46 @@
-﻿// принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
-string Palindrom(string str)
-{
-    int n = str.Length;
-    int count = 0;
-    int half = n / 2;
-    while (count < half)
+﻿// // принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// string Palindrom(string str)
+// {
+//     int n = str.Length;
+//     int count = 0;
+//     int half = n / 2;
+//     while (count < half)
+//     {
+//         if (str[count] == str[n-1])
+//         {
+//             n--; 
+//             count++;      
+//         }  
+//         else
+//             break;
+//     }
+//     if (half == count)
+//         return "Пaлиндром";
+//     else
+//         return "No palidrom";
+// }
+
+// Console.WriteLine("Wrie number:");
+// Console.WriteLine(Palindrom(Console.ReadLine()));
+
+
+string Palindrom(int n)
+{ 
+    int num = n / 10000;
+    int num1 = n % 10;
+    if (num == num1)
     {
-        if (str[count] == str[n-1])
-        {
-            n--; 
-            count++;      
-        }  
+        n = (n - num * 10000)/10;
+        num = n / 100;
+        num1 = n % 10;
+        if (num == num1)
+            return "Palidrom";
         else
-            break;
-    }
-    if (half == count)
-        return "Palidrom";
+            return "No palidron";
+    }   
     else
-        return "No palidrom";
+        return "No palidrom";    
 }
 
-Console.WriteLine("Wrie number:");
-Console.WriteLine(Palindrom(Console.ReadLine()));
+Console.WriteLine("Write number:");
+Console.WriteLine(Palindrom(int.Parse(Console.ReadLine())));
