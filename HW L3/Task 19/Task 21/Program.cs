a7a4348fa12df9f3ed7183bf46af5f617b351e98
Sergey <sergeileonov@inbox.ru>
2022-12-03 15:38:19 +0000

    принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N

---
+++ HW L3/Task 19/Task 21/Program.cs
@@ -0,0 +1,23 @@
+﻿// принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+void Tabl(int n)
+{
+    int count = 1;
+    while (count <= n)
+        {
+            if (count == n)
+            {
+                double kub = Math.Pow(count,3);
+                Console.Write($"{kub}");
+                Console.WriteLine();
+            }
+            else
+            {
+                double kub = Math.Pow(count,3);
+                Console.Write($"{kub}, ");
+            }
+
+            count +=1;
+        }
+}
+Console.WriteLine("Write number:");
+Tabl(int.Parse(Console.ReadLine()));
