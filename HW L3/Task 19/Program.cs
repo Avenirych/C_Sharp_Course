9d72c71e4b6031814e06834263acfb7af94667c9
Sergey <sergeileonov@inbox.ru>
2022-12-03 14:28:53 +0000

    принимает на вход пятизначное число и проверяет, является ли оно палиндромом

---
+++ HW L3/Task 19/Program.cs
@@ -0,0 +1,24 @@
+﻿// принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+string Palindrom(string str)
+{
+    int n = str.Length;
+    int count = 0;
+    int half = n / 2;
+    while (count < half)
+    {
+        if (str[count] == str[n-1])
+        {
+            n--; 
+            count++;      
+        }  
+        else
+            break;
+    }
+    if (half == count)
+        return "Palidrom";
+    else
+        return "No palidrom";
+}
+
+Console.WriteLine("Wrie number:");
+Console.WriteLine(Palindrom(Console.ReadLine()));
